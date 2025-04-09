d6afd73195ca917d8ab1efeced1e41e2e5f1f602
Peter LaValle <peter.lavalle@gmail.com>
2025-04-09 20:56:59 +0100

    input-dir argument syntax matches output-dir
    still need docs and adjusting the required arguments

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -19,7 +19,7 @@ def test_no_args():
     # check click results
     assert 2 == result.exit_code
     assert [
-        "Usage: mapstream [OPTIONS] [INPUT_DIR]...",
+        "Usage: mapstream [OPTIONS]",
         "Try 'mapstream --help' for help.",
         "",
         "Error: Missing option '--rules-file'.",
@@ -68,6 +68,7 @@ def test_with_example(tmp_path: Path, caplog):
     result = runner.invoke(
         mapstream,
         [
+            "--input-dir",
             f"{tmp_path}",
             "--rules-file",
             f"{rules}",
