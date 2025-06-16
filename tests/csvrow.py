2b8a95c0c07690cc8aabf964b54e1e4efd1da638
Peter LaValle <peter.lavalle@gmail.com>
2025-06-16 17:01:39 +0100

    "steps on" #63
    finds a case of not handling missing gender id

--- tests/csvrow.py
+++ tests/csvrow.py
@@ -59,7 +59,7 @@ def csv_rows(path, delimiter=","):
 
                 def __str__(self):
                     return str(self.__dict__)
-                    
+
                 def __repr__(self):
                     return self.__str__()
 
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -67,11 +67,17 @@ def test_integration_test1(tmp_path: Path):
     # target -> source
     [s2t, t2s] = csvrow.back_get(output / "person_ids.tsv")
 
+    assert 4 == len(s2t)
+
     ##
     # check the birthdays and gender
+    seen_ids = []
     for person in csvrow.csv_rows(output / "person.tsv", "\t"):
         # check the ids
         assert person.person_id in t2s
+        tid = t2s[person.person_id]
+        assert tid not in seen_ids
+        seen_ids.append(tid)
 
         # locate the source record
         src_person = src_persons[t2s[person.person_id]]
@@ -105,6 +111,10 @@ def test_integration_test1(tmp_path: Path):
             raise Exception(
                 f"unknown gender_source_value `{person.gender_source_value}`"
             )
+    assert "321" in seen_ids
+    assert "789345" in seen_ids
+    assert "6789" in seen_ids
+    assert "289" in seen_ids
 
     ##
     # check measurements
@@ -157,6 +167,18 @@ def test_integration_test1(tmp_path: Path):
     for observation in csvrow.csv_rows(output / "observation.tsv", "\t"):
         print(observation.observation_concept_id)
 
+        assert "0" == observation.observation_type_concept_id
+        assert "" == observation.value_as_number
+        assert "" == observation.value_as_concept_id
+        assert "" == observation.qualifier_concept_id
+        assert "" == observation.unit_concept_id
+        assert "" == observation.provider_id
+        assert "" == observation.visit_occurrence_id
+        assert "" == observation.visit_detail_id
+        assert "" == observation.unit_source_value
+        assert "" == observation.qualifier_source_value
+        assert observation.value_as_string == observation.observation_source_value
+
         if "35810208" == observation.observation_concept_id:
             assert_datetimes(
                 expected="2025-05-21 15:02",
@@ -164,20 +186,7 @@ def test_integration_test1(tmp_path: Path):
                 onlydate=observation.observation_date,
             )
 
-            assert "0" == observation.observation_type_concept_id
-            assert "" == observation.value_as_number
             assert "CURRENT_SMOKER" == observation.value_as_string
-            assert "" == observation.value_as_concept_id
-            assert "" == observation.qualifier_concept_id
-            assert "" == observation.unit_concept_id
-            assert "" == observation.provider_id
-            assert "" == observation.visit_occurrence_id
-            assert "" == observation.visit_detail_id
-            assert "CURRENT_SMOKER" == observation.observation_source_value
-            assert "35810208" == observation.observation_source_concept_id
-            assert "" == observation.unit_source_value
-            assert "" == observation.qualifier_source_value
-
             assert "789345" == t2s[observation.person_id]
 
         elif "35810209" == observation.observation_concept_id:
@@ -186,20 +195,8 @@ def test_integration_test1(tmp_path: Path):
                 datetime=observation.observation_datetime,
                 onlydate=observation.observation_date,
             )
-            assert "0" == observation.observation_type_concept_id
-            assert "" == observation.value_as_number
-            assert "FORMER_SMOKER" == observation.value_as_string
-            assert "" == observation.value_as_concept_id
-            assert "" == observation.qualifier_concept_id
-            assert "" == observation.unit_concept_id
-            assert "" == observation.provider_id
-            assert "" == observation.visit_occurrence_id
-            assert "" == observation.visit_detail_id
-            assert "FORMER_SMOKER" == observation.observation_source_value
-            assert "35810209" == observation.observation_source_concept_id
-            assert "" == observation.unit_source_value
-            assert "" == observation.qualifier_source_value
 
+            assert "FORMER_SMOKER" == observation.value_as_string
             assert "6789" == t2s[observation.person_id]
 
         elif "35821355" == observation.observation_concept_id:
@@ -208,29 +205,13 @@ def test_integration_test1(tmp_path: Path):
                 datetime=observation.observation_datetime,
                 onlydate=observation.observation_date,
             )
-            assert "0" == observation.observation_type_concept_id
-            assert "" == observation.value_as_number
-            assert "NEVER_SMOKER" == observation.value_as_string
-            assert "" == observation.value_as_concept_id
-            assert "" == observation.qualifier_concept_id
-            assert "" == observation.unit_concept_id
-            assert "" == observation.provider_id
-            assert "" == observation.visit_occurrence_id
-            assert "" == observation.visit_detail_id
-            assert "NEVER_SMOKER" == observation.observation_source_value
-            assert "35821355" == observation.observation_source_concept_id
-            assert "" == observation.unit_source_value
-            assert "" == observation.qualifier_source_value
 
+            assert "NEVER_SMOKER" == observation.value_as_string
             assert "321" == t2s[observation.person_id]
         else:
             raise Exception(
                 f"Unexpected observation.observation_concept_id `{observation.observation_concept_id}`"
             )
-
-
-
-
 
 
 @pytest.mark.unit
@@ -247,7 +228,6 @@ def test_duplications(tmp_path: Path):
     # setup the args
     arg__input_dir = test_files
     arg__rules_file = test_files / "transform-rules.json"
-    arg__person_file = test_files / "src_PERSON.csv"
 
     ##
     #
@@ -263,7 +243,6 @@ def test_duplications(tmp_path: Path):
     ##
     #
 
-
     # test the person_ids table
     [s2t, t2s] = csvrow.back_get(output / "person_ids.tsv")
 
@@ -273,8 +252,7 @@ def test_duplications(tmp_path: Path):
     # load and chcekc the output person(s)
     person_counts = {}
     people = {}
-    for person in csvrow.csv_rows(output / "person.tsv", '\t'):
-
+    for person in csvrow.csv_rows(output / "person.tsv", "\t"):
         src_person_id = t2s[person.person_id]
 
         if src_person_id not in people:
@@ -285,13 +263,12 @@ def test_duplications(tmp_path: Path):
             person_counts[src_person_id] += 1
 
     assert 3 == len(person_counts)
-    assert 2 == person_counts['321'] # there should be a single one here
-    assert 1 == person_counts['789345']
-    assert 1 == person_counts['6789']
+    assert 2 == person_counts["321"]  # there should be a single one here
+    assert 1 == person_counts["789345"]
+    assert 1 == person_counts["6789"]
 
     # load all of the src_PERSON.csv
     for src_person in csvrow.csv_rows(arg__input_dir / "src_PERSON.csv"):
-
         assert person_counts[src_person.person_id] > 0
         person_counts[src_person.person_id] -= 1
 
@@ -333,127 +310,125 @@ def test_duplications(tmp_path: Path):
     for k in person_counts:
         assert 0 == person_counts[k]
 
-
-    for measurement in csvrow.csv_rows(output / "measurement.tsv", '\t'):
-
+    for measurement in csvrow.csv_rows(output / "measurement.tsv", "\t"):
         person = people[t2s[measurement.person_id]]
 
         src_person_id = t2s[measurement.person_id]
 
-        assert '35811769' == measurement.measurement_concept_id
-        
+        assert "35811769" == measurement.measurement_concept_id
+
         # >>>
         # >>>
         # >>>
-        print('heyheyh hey !!! should this be blank?')
-        assert '' == measurement.measurement_time #
+        print("heyheyh hey !!! should this be blank?")
+        assert "" == measurement.measurement_time  #
         # >>>
         # >>>
         # >>>
 
-
-        assert '0' == measurement.measurement_type_concept_id
-        assert '' == measurement.value_as_concept_id
-        assert '' == measurement.unit_concept_id
-        assert '' == measurement.range_low
-        assert '' == measurement.range_high
-        assert '' == measurement.provider_id
-        assert '' == measurement.visit_occurrence_id
-        assert '' == measurement.visit_detail_id
-        assert '' == measurement.operator_concept_id
-        assert '35811769' == measurement.measurement_source_concept_id
-        assert '' == measurement.unit_source_value
-        assert '' == measurement.value_source_value
+        assert "0" == measurement.measurement_type_concept_id
+        assert "" == measurement.value_as_concept_id
+        assert "" == measurement.unit_concept_id
+        assert "" == measurement.range_low
+        assert "" == measurement.range_high
+        assert "" == measurement.provider_id
+        assert "" == measurement.visit_occurrence_id
+        assert "" == measurement.visit_detail_id
+        assert "" == measurement.operator_concept_id
+        assert "35811769" == measurement.measurement_source_concept_id
+        assert "" == measurement.unit_source_value
+        assert "" == measurement.value_source_value
         assert measurement.measurement_date == measurement.measurement_datetime[:10]
         assert measurement.value_as_number == measurement.measurement_source_value
-        
-        if '321' == src_person_id:
-            assert '7' == measurement.measurement_id
-            assert '2025-01-03 00:00:00' == measurement.measurement_datetime
-            assert '23' == measurement.value_as_number
 
-        elif '6789' == src_person_id:
+        if "321" == src_person_id:
+            assert "7" == measurement.measurement_id
+            assert "2025-01-03 00:00:00" == measurement.measurement_datetime
+            assert "23" == measurement.value_as_number
 
-            if '1' == measurement.measurement_id:
-                assert '2023-10-12 00:00:00' == measurement.measurement_datetime
-                assert '75' == measurement.value_as_number
-            
-            elif '2' == measurement.measurement_id:
-                assert '2023-10-11 00:00:00' == measurement.measurement_datetime
-                assert '76' == measurement.value_as_number
+        elif "6789" == src_person_id:
+            if "1" == measurement.measurement_id:
+                assert "2023-10-12 00:00:00" == measurement.measurement_datetime
+                assert "75" == measurement.value_as_number
 
-            elif '3' == measurement.measurement_id:
-                assert '2023-11-21 00:00:00' == measurement.measurement_datetime
-                assert '86' == measurement.value_as_number
+            elif "2" == measurement.measurement_id:
+                assert "2023-10-11 00:00:00" == measurement.measurement_datetime
+                assert "76" == measurement.value_as_number
 
-            elif '4' == measurement.measurement_id:
-                assert '2023-11-21 00:00:00' == measurement.measurement_datetime
-                assert '68' == measurement.value_as_number
-            
-            elif '5' == measurement.measurement_id:
-                assert '2023-11-21 00:00:00' == measurement.measurement_datetime
-                assert '86' == measurement.value_as_number
-                
-            elif '6' == measurement.measurement_id:
-                assert '2023-11-21 00:00:00' == measurement.measurement_datetime
-                assert '86' == measurement.value_as_number
-                
+            elif "3" == measurement.measurement_id:
+                assert "2023-11-21 00:00:00" == measurement.measurement_datetime
+                assert "86" == measurement.value_as_number
+
+            elif "4" == measurement.measurement_id:
+                assert "2023-11-21 00:00:00" == measurement.measurement_datetime
+                assert "68" == measurement.value_as_number
+
+            elif "5" == measurement.measurement_id:
+                assert "2023-11-21 00:00:00" == measurement.measurement_datetime
+                assert "86" == measurement.value_as_number
+
+            elif "6" == measurement.measurement_id:
+                assert "2023-11-21 00:00:00" == measurement.measurement_datetime
+                assert "86" == measurement.value_as_number
+
             else:
-                raise Exception(f'unexpected measurement {measurement=}')
+                raise Exception(f"unexpected measurement {measurement=}")
         else:
-            raise Exception('unexpected ')
+            raise Exception("unexpected ")
 
     ##
     # now check the observations
-    observation_count={}
+    observation_count = {}
     for k in s2t:
         observation_count[k] = 0
-    for observation in csvrow.csv_rows(output / "observation.tsv", '\t'):
-
+    for observation in csvrow.csv_rows(output / "observation.tsv", "\t"):
         person = people[t2s[observation.person_id]]
 
         src_person_id = t2s[observation.person_id]
         observation_count[src_person_id] += 1
 
-        assert '0' == observation.observation_type_concept_id
-        assert '' == observation.value_as_number
+        assert "0" == observation.observation_type_concept_id
+        assert "" == observation.value_as_number
         assert observation.observation_date == observation.observation_datetime[:10]
-        assert '' == observation.value_as_concept_id
-        assert '' == observation.qualifier_concept_id
-        assert '' == observation.unit_concept_id
-        assert '' == observation.provider_id
-        assert '' == observation.visit_occurrence_id
-        assert '' == observation.visit_detail_id
-        assert '' == observation.unit_source_value
-        assert '' == observation.qualifier_source_value
+        assert "" == observation.value_as_concept_id
+        assert "" == observation.qualifier_concept_id
+        assert "" == observation.unit_concept_id
+        assert "" == observation.provider_id
+        assert "" == observation.visit_occurrence_id
+        assert "" == observation.visit_detail_id
+        assert "" == observation.unit_source_value
+        assert "" == observation.qualifier_source_value
 
-
-        assert observation.observation_concept_id == observation.observation_source_concept_id
+        assert (
+            observation.observation_concept_id
+            == observation.observation_source_concept_id
+        )
         assert observation.value_as_string == observation.observation_source_value
 
-        if '789345' == src_person_id:
-            assert '35810208' == observation.observation_concept_id
-            assert '2025-05-21 15:02:00' == observation.observation_datetime
-            assert 'CURRENT_SMOKER' == observation.value_as_string
-            
-        elif '6789' ==  src_person_id:
-            assert '35810209' == observation.observation_concept_id
-            assert '2025-05-12 21:20:00' == observation.observation_datetime
-            assert 'FORMER_SMOKER' == observation.value_as_string
-            
-        elif '321' ==  src_person_id:
-            assert '35821355' == observation.observation_concept_id
-            assert '2025-05-22 03:14:00' == observation.observation_datetime
-            assert 'NEVER_SMOKER' == observation.value_as_string
+        if "789345" == src_person_id:
+            assert "35810208" == observation.observation_concept_id
+            assert "2025-05-21 15:02:00" == observation.observation_datetime
+            assert "CURRENT_SMOKER" == observation.value_as_string
+
+        elif "6789" == src_person_id:
+            assert "35810209" == observation.observation_concept_id
+            assert "2025-05-12 21:20:00" == observation.observation_datetime
+            assert "FORMER_SMOKER" == observation.value_as_string
+
+        elif "321" == src_person_id:
+            assert "35821355" == observation.observation_concept_id
+            assert "2025-05-22 03:14:00" == observation.observation_datetime
+            assert "NEVER_SMOKER" == observation.value_as_string
 
         else:
             raise Exception(
-                f'unexpected person {src_person_id=} {observation.person_id=}'
+                f"unexpected person {src_person_id=} {observation.person_id=}"
             )
 
-    assert 1 == observation_count['789345']
-    assert 1 == observation_count['6789']
-    assert 4 == observation_count['321']
+    assert 1 == observation_count["789345"]
+    assert 1 == observation_count["6789"]
+    assert 4 == observation_count["321"]
+
 
 def assert_datetimes(onlydate: str, datetime: str, expected: str):
     assert re.fullmatch(r"\d{4}-\d{2}-\d{2}", onlydate), (
