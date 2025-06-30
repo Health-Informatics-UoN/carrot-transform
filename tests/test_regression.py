728f82de1839ea1b0ff9e941b55f7cb35341f019
Peter LaValle <peter.lavalle@gmail.com>
2025-06-30 14:31:26 +0100

    another test with ... no gender

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -22,7 +22,7 @@ import csvrow
 
 
 @pytest.mark.unit
-def test_lazy_user(tmp_path: Path):
+def test_original_problem(tmp_path: Path):
     """
     the users are imported lazily - that needs to be fixed
 
@@ -42,11 +42,6 @@ def test_lazy_user(tmp_path: Path):
     arg__input_dir = assets / 'regression/lazy-user'
     arg__rules_file = arg__input_dir / 'transform-rules.json'
 
-    for i in range(1, 4):
-        print(
-            f"output in {str(tmp_path).replace('/','\\')}"
-        )
-
     ##
     #
     (result, output) = clicktools.click_mapstream(
@@ -62,6 +57,76 @@ def test_lazy_user(tmp_path: Path):
 
     assert 0 == result.exit_code
 
+    print(
+        f"out is {tmp_path=}"
+    )
+
+    ### check data
+
+    ##
+    # backmap the ids.
+    # source -> target
+    # target -> source
+    [s2t, t2s] = csvrow.back_get(output / "person_ids.tsv")
+
+    assert 6 == len(s2t), "shouldbe 5 people in the csv"
+
+    # just check the 
+    seen_ids = []
+    for person in csvrow.csv_rows(output / "person.tsv", "\t"):
+        # check the ids
+        assert person.person_id in t2s
+        tid = t2s[person.person_id]
+        assert tid not in seen_ids
+        seen_ids.append(tid)
+
+        print(
+            f" {person.person_id=}"
+        )
+
+    assert "321" in seen_ids
+    assert "789345" in seen_ids
+    assert "6789" in seen_ids
+    assert "339" in seen_ids, "this id was unused but has a gender"
+    assert "319" in seen_ids, "this id is used with a blank gender"
+    assert "289" in seen_ids, "this id is unused and has no gender"
+
+    assert 6 == len(seen_ids), "there are extra persons"
+
+
+@pytest.mark.unit
+def test_gender_as_race(tmp_path: Path):
+    """
+    it's the lazy user tests - but - the gender values are mapped to "race"
+
+    this fails for a new reasson
+    """
+
+    
+
+    # Get the package root directory
+    assets = importlib.resources.files("tests")
+    
+    arg__input_dir = assets / 'regression/lazy-user'
+    arg__rules_file = arg__input_dir / 'gender-as-race.json'
+
+    ##
+    #
+    (result, output) = clicktools.click_mapstream(
+        tmp_path,
+        ["src_PERSON.csv", "src_SMOKING.csv", "src_WEIGHT.csv"],
+        arg__input_dir,
+        arg__rules_file
+    )
+
+
+
+    if None is not result.exception:
+        print(result.output)
+        raise result.exception
+
+    assert 0 == result.exit_code
+    
     ### check data
 
     ##
