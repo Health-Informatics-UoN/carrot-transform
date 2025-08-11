9ea4b350fee310648b7b808547a4f48566c26e41
Peter LaValle <peter.lavalle@gmail.com>
2025-08-11 17:05:09 +0100

    Merge remote-tracking branch 'origin/main' into sql-source

--- tests/clicktools.py
+++ tests/clicktools.py
@@ -1,7 +1,6 @@
 import importlib.resources
 
 from pathlib import Path
-import shutil
 
 from click.testing import CliRunner
 from carrottransform.cli.subcommands.run import mapstream
@@ -70,40 +69,6 @@ def click_generic(
     )
 
     return (result, output, person_id_source2target, person_id_target2source)
-
-
-def click_example(tmp_path: Path, limit: int = -1):
-    """sets up the/a test environment and runs the transform thing with it."""
-
-    # Get the package root directory
-    package_root = importlib.resources.files("carrottransform")
-    package_root = (
-        package_root if isinstance(package_root, Path) else Path(str(package_root))
-    )
-
-    # rules from carrot mapper
-    rules_src = package_root / "examples/test/rules/rules_14June2021.json"
-    rules = tmp_path / "rules.json"
-    shutil.copy2(rules_src, rules)
-
-    srcs = [
-        "Demographics.csv",
-        "covid19_antibody.csv",
-        "Covid19_test.csv",
-        "Symptoms.csv",
-        "vaccine.csv",
-    ]
-
-    # the source files
-    for src in srcs:
-        with (
-            open(package_root / "examples/test/inputs" / src) as s,
-            open(tmp_path / src, "w") as o,
-        ):
-            for s in s.readlines() if limit < 0 else s.readlines()[: (limit + 1)]:
-                o.write(s)
-
-    return click_mapstream(tmp_path, srcs, tmp_path, rules)
 
 
 def click_mapstream(tmp_path: Path, src_names, src_from, rules: Path):
