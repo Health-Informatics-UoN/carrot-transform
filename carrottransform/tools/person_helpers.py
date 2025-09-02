aaa81fad0356bfa4c010c531219b7c46a263c4f5
AndrewThien <anretrithien@gmail.com>
2025-09-02 16:38:15 +0100

    separate load person ID functions

--- carrottransform/tools/person_helpers.py
+++ carrottransform/tools/person_helpers.py
@@ -26,7 +26,7 @@ def load_last_used_ids(last_used_ids_file: Path, last_used_ids):
     return last_used_ids
 
 
-def load_person_ids(
+def load_person_ids_v2(
     saved_person_id_file,
     person_file: Path | None,
     person_table_name: str | None,
@@ -36,35 +36,6 @@ def load_person_ids(
     db_connection: Optional[Connection] = None,
     schema: Optional[str] = None,
 ):
-    """`old` loading method that accepts a Path object pointing to the file along witrh a delimeter.
-
-    this is used to preserve the old API for the current v2 testing
-    """
-    return read_person_ids(
-        saved_person_id_file=saved_person_id_file,
-        csvr=csv.reader(
-            person_file.open(mode="r", encoding="utf-8-sig"), delimiter=delim
-        ),
-        mappingrules=mappingrules,
-        use_input_person_ids=use_input_person_ids,
-    )
-
-
-def read_person_ids(
-    saved_person_id_file: Path,
-    csvr: Iterable[list[str]],
-    mappingrules: MappingRules,
-    use_input_person_ids: bool,
-):
-    """revised loading method that accepts an itterator eitehr for a file or for a database connection"""
-
-    if not isinstance(use_input_person_ids, bool):
-        raise Exception(
-            f"use_input_person_ids needs to be bool but it was {type(use_input_person_ids)=}"
-        )
-    if not isinstance(csvr, Iterable):
-        raise Exception(f"csvr needs to be iterable but it was {type(csvr)=}")
-
     person_ids, person_number = _get_person_lookup(saved_person_id_file)
 
     if db_connection and person_table_name:
@@ -111,7 +82,7 @@ def read_person_ids(
         if (
             persondata[person_col] not in person_ids
         ):  # if not already in person_ids dict, add it
-            if not use_input_person_ids:
+            if use_input_person_ids == "N":
                 person_ids[persondata[person_col]] = str(
                     person_number
                 )  # create a new integer person_id
@@ -122,6 +93,90 @@ def read_person_ids(
                 )  # use existing person_id
     if not db_connection:
         fh.close()
+
+    return person_ids, reject_count
+
+
+def load_person_ids(
+    saved_person_id_file: Path,
+    person_file: Path,
+    mappingrules: MappingRules,
+    use_input_person_ids: bool,
+    delim: str = ",",
+):
+    """`old` loading method that accepts a Path object pointing to the file along witrh a delimeter.
+
+    this is used to preserve the old API for the current v2 testing
+    """
+    return read_person_ids(
+        saved_person_id_file=saved_person_id_file,
+        csvr=csv.reader(
+            person_file.open(mode="r", encoding="utf-8-sig"), delimiter=delim
+        ),
+        mappingrules=mappingrules,
+        use_input_person_ids=use_input_person_ids,
+    )
+
+
+def read_person_ids(
+    saved_person_id_file: Path,
+    csvr: Iterable[list[str]],
+    mappingrules: MappingRules,
+    use_input_person_ids: bool,
+):
+    """revised loading method that accepts an itterator eitehr for a file or for a database connection"""
+
+    if not isinstance(use_input_person_ids, bool):
+        raise Exception(
+            f"use_input_person_ids needs to be bool but it was {type(use_input_person_ids)=}"
+        )
+    if not isinstance(csvr, Iterable):
+        raise Exception(f"csvr needs to be iterable but it was {type(csvr)=}")
+
+    person_ids, person_number = _get_person_lookup(saved_person_id_file)
+
+    person_columns = {}
+    person_col_in_hdr_number = 0
+    reject_count = 0
+    # Header row of the person file
+    personhdr = next(csvr)
+    # TODO: not sure if this is needed
+    logger.info("Headers in Person file: %s", personhdr)
+
+    # Make a dictionary of column names vs their positions
+    for col in personhdr:
+        person_columns[col] = person_col_in_hdr_number
+        person_col_in_hdr_number += 1
+
+    ## check the mapping rules for person to find where to get the person data) i.e., which column in the person file contains dob, sex
+    birth_datetime_source, person_id_source = mappingrules.get_person_source_field_info(
+        "person"
+    )
+
+    ## get the column index of the PersonID from the input file
+    person_col = person_columns[person_id_source]
+
+    for persondata in csvr:
+        if not valid_value(
+            persondata[person_columns[person_id_source]]
+        ):  # just checking that the id is not an empty string
+            reject_count += 1
+            continue
+        if not valid_date_value(persondata[person_columns[birth_datetime_source]]):
+            reject_count += 1
+            continue
+        if (
+            persondata[person_col] not in person_ids
+        ):  # if not already in person_ids dict, add it
+            if not use_input_person_ids:
+                person_ids[persondata[person_col]] = str(
+                    person_number
+                )  # create a new integer person_id
+                person_number += 1
+            else:
+                person_ids[persondata[person_col]] = str(
+                    persondata[person_col]
+                )  # use existing person_id
 
     return person_ids, reject_count
 
