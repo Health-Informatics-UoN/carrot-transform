bdd0f044fec86117c02876055b40689a879aabc9
James Mitchell-White <jamesjimitchell@gmail.com>
2026-01-16 09:19:24 +0000

    extra models for V2 rulesets

--- carrottransform/tools/mapping_types.py
+++ carrottransform/tools/mapping_types.py
@@ -1,26 +1,43 @@
+from datetime import datetime
+from typing import Literal
 from pydantic import BaseModel
+
+class RuleSetMetadata(BaseModel):
+    date_created: datetime
+    dataset: str
 
 # To prevent circular import, these types should be in a separate file rather than in the types.py
 class PersonIdMapping(BaseModel):
     source_field: str
     dest_field: str
 
-
 class DateMapping(BaseModel):
     source_field: str
     dest_fields: list[str]
 
-
 class ConceptMapping(BaseModel):
     source_field: str
+    # if I had my druthers, this would be better as {value: somevalue, dest_field: somefield, concept_ids: list[int]} triples
     value_mappings: dict[
         str, dict[str, list[int]]
     ]  # value -> dest_field -> concept_ids
-    original_value_fields: list[str]
+    original_value: list[str]
 
+class V1CDMField(BaseModel):
+    ...
+    
 
 class V2TableMapping(BaseModel):
-    source_table: str
+    # e.g. "Symptoms.csv"
     person_id_mapping: PersonIdMapping | None
     date_mapping: DateMapping | None
     concept_mappings: dict[str, ConceptMapping]  # source_field -> ConceptMapping
+
+class V2RuleSet(BaseModel):
+    metadata: RuleSetMetadata
+    # an array of values with a destination_table field would be easier to parse
+    cdm: dict[Literal["observation", "measurement", "person", "condition_occurrence"], V2TableMapping]
+
+class V1RuleSet(BaseModel):
+    metadata: RuleSetMetadata
+    cdm: None
