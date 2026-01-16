52dc3c3615404af1934be63f986e4d5f4d918f16
James Mitchell-White <jamesjimitchell@gmail.com>
2026-01-16 09:49:46 +0000

    RuleSet models get methods

--- carrottransform/tools/mapping_types.py
+++ carrottransform/tools/mapping_types.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Literal
 from pydantic import BaseModel
+import json
 
 class RuleSetMetadata(BaseModel):
     date_created: datetime
@@ -31,7 +32,6 @@ class V1CDMField(BaseModel):
     
 
 class V2TableMapping(BaseModel):
-    # e.g. "Symptoms.csv"
     person_id_mapping: PersonIdMapping | None
     date_mapping: DateMapping | None
     concept_mappings: dict[str, ConceptMapping]  # source_field -> ConceptMapping
@@ -39,8 +39,71 @@ class V2TableMapping(BaseModel):
 class V2RuleSet(BaseModel):
     metadata: RuleSetMetadata | None
     # an array of values with a destination_table field would be easier to parse
-    cdm: dict[Literal["observation", "measurement", "person", "condition_occurrence"], V2TableMapping]
+    cdm: dict[Literal["observation", "measurement", "person", "condition_occurrence"], dict[str, V2TableMapping]]
+    
+    def dump_parsed_rules(self) -> str:
+        return json.dumps(self.cdm)
 
+    def get_dsname_from_rules(self) -> str:
+        if self.metadata is None:
+            return "Unknown"
+        else:
+            return self.metadata.dataset
+
+    def get_all_outfile_names(self) -> list[str]:
+        return list(self.cdm.keys())
+
+    def get_all_infile_names(self) -> list[str]:
+        keys = [mapping.keys() for mapping in self.cdm.values()]
+        return list(set(*keys))
+
+    def get_infile_data_fields(self, infilename: str) -> dict[str, list[str]]:
+        ...
+
+    def get_infile_date_person_id(self, infilename: str) -> tuple[str,str]:
+        ...
+
+    def get_person_source_field_info(self, tgtfilename: str) -> tuple[Optional[str], Optional[str]]:
+        ...
+
+    def parse_rules_src_to_tgt(self, infilename: str) -> tuple[list[str], dict[Any]]:
+        # Aargh what is this
+        ...
+
+    def process_rules(self, infilename, outfilename, rules):
+        ...
+    
 class V1RuleSet(BaseModel):
     metadata: RuleSetMetadata | None
     cdm: dict[Literal["observation", "measurement", "person", "condition_occurrence"], dict[str, V1CDMField]]
+
+    def dump_parsed_rules(self) -> str:
+        return json.dumps(self.cdm)
+
+    def get_dsname_from_rules(self) -> str:
+        if self.metadata is None:
+            return "Unknown"
+        else:
+            return self.metadata.dataset
+
+    def get_all_outfile_names(self) -> list[str]:
+        ...
+
+    def get_all_infile_names(self) -> list[str]:
+        ...
+
+    def get_infile_data_fields(self, infilename: str) -> dict[str, list[str]]:
+        ...
+
+    def get_infile_date_person_id(self, infilename: str) -> tuple[str,str]:
+        ...
+
+    def get_person_source_field_info(self, tgtfilename: str) -> tuple[Optional[str], Optional[str]]:
+        ...
+
+    def parse_rules_src_to_tgt(self, infilename: str) -> tuple[list[str], dict[Any]]:
+        # Aargh what is this
+        ...
+
+    def process_rules(self, infilename, outfilename, rules):
+        ...
